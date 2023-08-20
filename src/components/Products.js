be2f4685ba3797ebf04439a00f8f5e7873b88d21
Zahra Bahmanpour <z.bmnpr@gmail.com>
2023-08-20 08:26:38 +0330

    add isError

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,11 +2,16 @@ import { useGetProductsQuery } from "../features/product/productApiSlice";
 import ProductItem from "./ProductItem";
 
 const Products = () => {
-  const { data: products, isLoading } = useGetProductsQuery();
+  const { data: products, isLoading, isError, error } = useGetProductsQuery();
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
+
+  if (isError) {
+    return <div>{error.status}</div>;
+  }
+
   return products.map((product) => (
     <ProductItem key={product.id} {...product} />
   ));
